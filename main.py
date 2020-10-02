622adf1652a30c09143faf7493a25ce21cdce251
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-10-02 14:42:03 -0300

    region

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ out.write('inicio\n\n')
 for file in files:
     if 'sBoticsR' in file:
         a = open('./src/' + file, 'r')
+        out.write('##region ' + file.strip('.sBoticsR') + '\n')
         for linha in a:
             if '#' in linha:
                 pass
@@ -19,11 +20,11 @@ for file in files:
                 print(linha, end='')
                 out.write('\t' + linha)
         a.close()
+        out.write('\n##endregion\n\n')
         
     else:
         print('-'*20, 'Arquivo inválido','-'*20, end='')
     print('\n','-'*20, 'fim do arquivo','-'*20)
-    out.write('\n')
 
 out.write('\nfim\n')
 out.close()
