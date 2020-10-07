91a1fa87ecdbda129cf476d9a924ed79b3c60828
gabrieldp23 <66735014+gabrieldp23@users.noreply.github.com>
2020-10-06 22:05:55 -0300

    Versão adaptada para c#
    Extensão ".sboticsC" porém pode ser trocada facilmente

---
+++ c#main.py
@@ -0,0 +1,35 @@
+import os
+
+os.system('code --install-extension maptz.regionfolder')
+
+while True:
+
+    diretorio = os.listdir('./src')
+
+    if 'output' not in os.listdir('./'):
+        os.mkdir('./output')
+
+    out = open('./output/main.sboticsC', 'w')
+    
+    os.system('cls')
+
+    for arquivo in diretorio:
+        if 'sboticsC' in arquivo:
+            arquivoAtual = open('./src/' + arquivo, 'r')
+            out.write('//' + arquivo[:arquivo.find('.sboticsC')] + '\n\n')
+            for linha in arquivoAtual:
+                if '// ' in linha:
+                    pass
+                else:
+                    out.write('\t' + linha)
+            arquivoAtual.close()
+            out.write('\n\n//\n\n')
+            
+        else:
+            print('-'*20, 'Arquivo inválido:', arquivo,'-'*20, end='')
+        print('\n','-'*20, 'concluído', '-'*20)
+
+    out.close()
+
+    while input() != 'compilar':
+        pass
