d3128bfa9dc6efe9b685f55396132de6d9c30295
Eduardo Santos Barreto <eduwagzozo@gmail.com>
2020-10-27 16:54:23 -0300

    regions retiradas e pasta output virou out

--- c#main.py
+++ c#main.py
@@ -1,36 +1,37 @@
 import os
 
-os.system('code --install-extension maptz.regionfolder')
+
+file_name = input('Digite o nome do arquivo final (sem a extensão): ')
+
 
 while True:
 
     directory = os.listdir('./src')
 
-    if 'output' not in os.listdir('./'):
-        os.mkdir('./output')
+    if 'out' not in os.listdir('./'):
+        os.mkdir('./out')
 
-    out = open('./output/main.sboticsC', 'w')
-    
+    out = open('./out/'+file_name+'.sboticsC', 'w')
+
     os.system('cls')
 
     for source_file in directory:
         if 'sboticsc' in source_file.lower() or 'cs' in source_file.lower():
             current_file = open('./src/' + source_file, 'r')
-            out.write('//region' + source_file[:source_file.lower().find('.sboticsc')] + '\n\n')
             for line in current_file:
-                if '// ' in line:
+                if '//' in line:
                     pass
                 else:
-                    out.write('\t' + line)
+                    out.write(line)
             current_file.close()
-            out.write('\n\n//region\n\n')
-            
+            out.write('\n\n')
+
         else:
             print('-'*20, 'Arquivo inválido:', source_file,'-'*20, end='')
         print('\n', '-'*20, 'Arquivo', source_file[:source_file.lower().find('.sboticsc')], 'compilado com sucesso', '-'*20)
 
     out.close()
-    
+
     print('\nDigite compilar para compilar novamente\n')
     while input() != 'compilar':
         pass
