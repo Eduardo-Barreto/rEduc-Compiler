9305f194d0157db83c988569764ecec47194f90c
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-10-27 17:02:49 -0300

    regions retiradas e pasta final chama out

--- main.py
+++ main.py
@@ -9,23 +9,19 @@ while True:
 
     directory = os.listdir('./src')
 
-    if 'output' not in os.listdir('./'):
-        os.mkdir('./output')
+    if 'out' not in os.listdir('./'):
+        os.mkdir('./out')
 
-    out = open('./output/'+file_name+'.sBoticsR', 'w')
+    out = open('./out/'+file_name+'.sBoticsR', 'w')
 
     os.system('cls')
 
     for source_file in directory:
         if 'sBoticsR' in source_file:
             current_file = open('./src/' + source_file, 'r')
-            out.write('#region ' +
-                      source_file[:source_file.find('.sBoticsR')] +
-                      '\n')
             for line in current_file:
                 out.write('\t' + line)
             current_file.close()
-            out.write('\n#endregion\n\n')
 
         else:
             print('-'*20, 'Arquivo inválido:', source_file, '-'*20, end='')
