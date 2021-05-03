008b3c9c79c055275ba2c0484470424a74f32cb0
Vinicios Lugli <vinicioslugli@gmail.com>
2021-05-03 14:53:43 -0300

    Fix import with tab before command

--- compiler.py
+++ compiler.py
@@ -11,98 +11,93 @@ out = None
 
 
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+	os.system('cls' if os.name == 'nt' else 'clear')
 # Classes ----------------------------------------------------------------
 
 
 class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKCYAN = '\033[96m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 
 class MainFileManager():
-    def __init__(self):
-        self.watch_dirs = ['out', 'src']
-        self.name = ""
-        self.command = ""
-        self.extension = ""
-        self.comment = ""
+	def __init__(self):
+		self.watch_dirs = ['out', 'src']
+		self.name = ""
+		self.command = ""
+		self.extension = ""
+		self.comment = ""
 
-    def checkExtension(self, name):
-        self.name = name
-        try:
-            self.extension = str(self.name.split('.')[1]).lower()
-        except IndexError:
-            print(
-                'Please restart the program and enter the name of the final ' +
-                'file as the example, with the extension.'
-            )
-            quit()
+	def checkExtension(self, name):
+		self.name = name
+		try:
+			self.extension = str(self.name.split('.')[1]).lower()
+		except IndexError:
+			print(
+				'Please restart the program and enter the name of the final ' +
+				'file as the example, with the extension.'
+			)
+			quit()
 
-    def language(self):
-        if(self.extension.lower() in ['sboticsr', 're']):
-            self.command, self.extension, self.comment = (
-                'importar', self.extension, '#'
-            )
-        else:
-            self.command, self.extension, self.comment = (
-                'import', self.extension, '//'
-            )
+	def language(self):
+		if(self.extension.lower() in ['sboticsr', 're']):
+			self.command, self.extension, self.comment = (
+				'importar', self.extension, '#'
+			)
+		else:
+			self.command, self.extension, self.comment = (
+				'import', self.extension, '//'
+			)
 
-    def diretory(self):
-        root = os.listdir('./')
-        for folder in self.watch_dirs:
-            if folder not in root:
-                os.mkdir(f'./{folder}')
+	def diretory(self):
+		root = os.listdir('./')
+		for folder in self.watch_dirs:
+			if folder not in root:
+				os.mkdir(f'./{folder}')
 
 
 class ImporterManager():
-    def include(self, out_name, import_name):
-        print(f'{colors.OKBLUE}importing: {import_name}{colors.ENDC}')
-        out_file = open(out_name, 'w', encoding='utf-8')
-        try:
-            file_to_import = open(import_name, 'r', encoding='utf-8')
-        except FileNotFoundError:
-            print(f'File {import_name} not found')
-            return
+	def include(self, out_name, import_name):
+		print(f'{colors.OKBLUE}importing: {import_name}{colors.ENDC}')
+		out_file = open(out_name, 'w', encoding='utf-8')
+		try:
+			file_to_import = open(import_name, 'r', encoding='utf-8')
+		except FileNotFoundError:
+			print(f'File {import_name} not found')
+			return
 
-        out.write(f'{MainFile.comment} file "{import_name}"\n')
+		out.write(f'{MainFile.comment} file "{import_name}"\n')
 
-        for line in file_to_import.readlines():
-            line = line.replace('\t', '')
-            if f'{MainFile.command}("' in line:
-                file_imported = line.replace(MainFile.command, '')
-                file_imported = file_imported.strip()[
-                    2:file_imported.find('")')]
-                file_imported = file_imported.replace(
-                    f'{MainFile.extension}', '')
-                file_imported = file_imported.strip('")')
-                file_imported = file_imported.strip('.')
-                file_imported = (
-                    f'./src/{file_imported}.' +
-                    f'{MainFile.extension}'
-                )
-                self.include(out_name, file_imported)
-            else:
-                out.write(line)
-        out.write(f'\n{MainFile.comment} endfile "{import_name}"\n')
-        file_to_import.close()
+		for line in file_to_import.readlines():
+			if line.find(MainFile.command) > -1:
+				file_imported = line.replace(MainFile.command, '')
+				file_imported = file_imported.replace(f'.{MainFile.extension}', '')
+				file_imported = file_imported[file_imported.find('("')+2:file_imported.find('")')]
+				file_imported = (
+					f'./src/{file_imported}.' +
+					f'{MainFile.extension}'
+				)
+				self.include(out_name, file_imported)
+			else:
+				out.write(line)
+		out.write(f'\n{MainFile.comment} endfile "{import_name}"\n')
+		file_to_import.close()
 
 
 # Instances ----------------------------------------------------------------
 MainFile = MainFileManager()
 MainFile.checkExtension(
-    input(
-        'Enter the name of the final file' +
-        '(with the extension, such as "main.cs"): '
-    )
+	input(
+		'Enter the name of the final file' +
+		'(with the extension, such as "main.cs"): '
+	)
 )
 MainFile.diretory()
 MainFile.language()
@@ -115,72 +110,50 @@ print("Waiting modifications...")
 
 
 def Process():
-    global out
-    clear()
-    data = datetime.datetime.now().replace(microsecond=0)
+	global out
+	clear()
+	data = datetime.datetime.now().replace(microsecond=0)
 
-    source_main = open(
-        f'./src/main.{MainFile.extension}',
-        'r',
-        encoding='utf-8'
-    )
-    out = open(f'./out/{MainFile.name}', 'w', encoding='utf-8')
-    for line in source_main.readlines():
-        if f'{MainFile.command}("' in line:
-            file_to_import = line.replace(MainFile.command, '')
-            file_to_import = file_to_import.strip()[
-                2:file_to_import.find('")')]
-            file_to_import = file_to_import.replace(
-                f'{MainFile.extension}', '')
-            file_to_import = file_to_import.strip('")')
-            file_to_import = file_to_import.strip('.')
-            file_to_import = (
-                f'./src/{file_to_import}.' +
-                f'{MainFile.extension}'
-            )
-            Importer.include(
-                f'./out/{MainFile.name}', file_to_import)
-        else:
-            out.write(line)
+	out = open(f'./out/{MainFile.name}', 'w', encoding='utf-8')
+	Importer.include(f'./out/{MainFile.name}', f'./src/main.{MainFile.extension}')
 
-    print(f'{colors.OKGREEN}Compiled at: {data.time()}{colors.ENDC}')
-    out.close()
-    source_main.close()
+	print(f'{colors.OKGREEN}Compiled at: {data.time()}{colors.ENDC}')
+	out.close()
 
 
 class Handler(FileSystemEventHandler):
 
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
+	@staticmethod
+	def on_any_event(event):
+		if event.is_directory:
+			return None
 
-        elif event.event_type == 'modified':
-            Process()
-            time.sleep(1)
+		elif event.event_type == 'modified':
+			Process()
+			time.sleep(1)
 
 
 class Watcher:
-    DIRECTORY_TO_WATCH = "./src"
+	DIRECTORY_TO_WATCH = "./src"
 
-    def __init__(self):
-        self.observer = Observer()
+	def __init__(self):
+		self.observer = Observer()
 
-    def run(self):
-        event_handler = Handler()
-        self.observer.schedule(
-            event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(5)
-        except:
-            self.observer.stop()
-            print("Error")
+	def run(self):
+		event_handler = Handler()
+		self.observer.schedule(
+			event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+		self.observer.start()
+		try:
+			while True:
+				time.sleep(5)
+		except:
+			self.observer.stop()
+			print("Error")
 
-        self.observer.join()
+		self.observer.join()
 
 
 if __name__ == '__main__':
-    w = Watcher()
-    w.run()
+	w = Watcher()
+	w.run()
